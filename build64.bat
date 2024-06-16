4f0f36e2ffa2a92a2d4d5a4265f17f7b99714205
chenj-bd <chenj-bd@glodon.com>
2024-06-16 20:41:53 +0800

    Build the test with rapidjson

--- build64.bat
+++ build64.bat
@@ -7,7 +7,7 @@ pushd %folder% >nul
 
 @echo on
 
-cmake -A x64 -DCPPZMQ_BUILD_TESTS=OFF ..
+cmake -A x64 -DBUILD_TESTS=OFF -DCPPZMQ_BUILD_TESTS=OFF -DRAPIDJSON_BUILD_TESTS=OFF ..
 cmake --build . --config %config%
 
 @echo off
--- zmq-req.cpp
+++ zmq-req.cpp
@@ -1,9 +1,30 @@
 #include "zmq.hpp"
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+using namespace std;
+using namespace zmq;
+using namespace rapidjson;
 
 int main()
 {
-    zmq::context_t context(1);
-    zmq::socket_t socket(context, ZMQ_REQ);
+    context_t context(1);
+    socket_t socket(context, ZMQ_REQ);
+    socket.connect("tcp://localhost:5555");
 
+    Document d;
+    d.SetObject();
+    d.AddMember("Name", "Ibrahim", d.GetAllocator());
+    d.AddMember("Age", 27, d.GetAllocator());
+    d.AddMember("Country", "Lebanon", d.GetAllocator());
 
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    string json_string = buffer.GetString();
+    printf("The string is %s\n", json_string.c_str());
+
+    socket.send(message_t(json_string), send_flags::dontwait);
 }
